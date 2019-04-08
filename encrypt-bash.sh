06efc4f5f646f85bc2bdde6a2f635a8961176fe8
mr-T <49389118+mr-Ting@users.noreply.github.com>
2019-04-08 19:27:07 +0700

    Add files via upload

---
+++ encrypt-bash.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# thanks Ancient One
+sudo apt-get install shc -y
+sleep 0.3
+clear
+echo "-------------------------------------------------------------------, "
+echo "created by mr -T                                                   ' "
+echo " _____                                                             ' "
+echo "|  ___|                                                            ' "
+echo "| |___                                                             ' "
+echo "|  ___|                                                            ' "
+echo "| |___                                                             ' "
+echo "|_____|NCRYPT-BASH                                                 ' "
+echo "-------------------------------------------------------------------' "
+echo "output: "
+echo "       1.namafile.sh.x"
+echo "       2.namafile.sh.x.c"
+echo ""
+read -p "ketikkan nama filenya :" nam;
+shc -f $nam
+ls
+sleep 5
+clear 
+echo "terimakasih telah menggunakan tools ini"
+sleep 2
+clear
+sleep 0.1
+exit
